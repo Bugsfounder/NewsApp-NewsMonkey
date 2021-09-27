af25bf4b3867f3d8030a36710432c4bf121701fd
Bugsfounder <82953561+Bugsfounder@users.noreply.github.com>
2021-09-27 01:43:33 -0700

    Fetching News Caracter wise

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
                <div className="card">
                     <img src={imageUrl?imageUrl:"https://images.wsj.net/im-407095/social"} className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
+                        <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}...</p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark btn-sm">Read More</a>
                     </div>
